2c08f65060541894d088b7dd73eb094deaa6ff9f
anllemonone <anl.bozkurt@lemonone.com>
2018-09-13 14:47:15 +0200

    compare commit

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -6,14 +6,14 @@ function SelectLanguage(props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
   return (
     <ul className="languages">
-      {languages.map(function(lang) {
+      {languages.map(lang => {
         return (
           <li
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}
             style={
               lang === props.selectedLanguage ? { color: '#d0021b' } : null
             }
-            onClick={props.onSelect.bind(null, lang)}
-            key={lang}
           >
             {lang}
           </li>
@@ -26,7 +26,7 @@ function SelectLanguage(props) {
 function RepoGrid(props) {
   return (
     <ul className="popular-list">
-      {props.repos.map(function(repo, index) {
+      {props.repos.map((repo, index) => {
         return (
           <li key={repo.name} className="popular-item">
             <div className="popular-rank">#{index + 1}</div>
@@ -42,7 +42,7 @@ function RepoGrid(props) {
                 <a href={repo.html_url}>{repo.name}</a>
               </li>
               <li>@{repo.owner.login}</li>
-              <li>{repo.stargazers_count} stars</li>
+              <li>{repo.stargazers_count} Stars</li>
             </ul>
           </li>
         );
@@ -51,18 +51,18 @@ function RepoGrid(props) {
   );
 }
 
-RepoGrid.propTypes = {
-  repos: PropTypes.array.isRequired
-};
-
 SelectLanguage.propTypes = {
   selectedLanguage: PropTypes.string.isRequired,
   onSelect: PropTypes.func.isRequired
 };
 
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+};
+
 class Popular extends React.Component {
   constructor(props) {
-    super();
+    super(props);
     this.state = {
       selectedLanguage: 'All',
       repos: null
@@ -70,27 +70,26 @@ class Popular extends React.Component {
 
     this.updateLanguage = this.updateLanguage.bind(this);
   }
+
   componentDidMount() {
     this.updateLanguage(this.state.selectedLanguage);
   }
+
   updateLanguage(lang) {
-    this.setState(function() {
+    this.setState(() => {
       return {
         selectedLanguage: lang,
         repos: null
       };
     });
 
-    api.fetchPopularRepos(lang).then(
-      function(repos) {
-        this.setState(function() {
-          return {
-            repos: repos
-          };
-        });
-      }.bind(this)
-    );
+    api.fetchPopularRepos(lang).then(repos => {
+      this.setState(() => {
+        repos;
+      });
+    });
   }
+
   render() {
     return (
       <div>
