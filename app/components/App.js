95e2db2bc3670f27ce5222d903b38eb401cd2fc3
Your Name <anl.bozkurt@lemonone.co>
2018-09-15 12:58:36 +0200

    3rd task

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import Popular from './Popular';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
+import { Switch } from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <Popular />
-      </div>
+      <BrowserRouter>
+        <div className="container">
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/battle" component={Battle} />
+            <Route path="/popular" component={Popular} />
+            <Route
+              render={() => {
+                return <p>Not Found</p>;
+              }}
+            />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
