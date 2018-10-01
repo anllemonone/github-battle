84320e24d12951657407d437c748af957fcebd7a
anllemonone <anl.bozkurt@lemonone.com>
2018-10-01 22:25:50 +0200

    ...

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import api from '../utils/Api';
+import Loading from './Loading';
 
 function SelectLanguage(props) {
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -100,7 +101,7 @@ class Popular extends React.Component {
           onSelect={this.updateLanguage}
         />
         {!this.state.repos ? (
-          <p>LOADING!</p>
+          <Loading />
         ) : (
           <RepoGrid repos={this.state.repos} />
         )}
