36062bcafff56e152d485ea889ceb205a9f4a638
anllemonone <anl.bozkurt@lemonone.com>
2018-10-18 18:57:00 +0200

    refactor api.js

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,26 +1,22 @@
 import axios from 'axios';
 
 const getProfile = username => {
-  return axios.get(`https://api.github.com/users/${username}`).then(user => {
-    return user.data;
-  });
+  return axios
+    .get(`https://api.github.com/users/${username}`)
+    .then(({ data }) => data);
 };
 
-const getRepos = username => {
-  return axios.get(`https://api.github.com/users/${username}/repos`);
-};
-
+const getRepos = username =>
+  axios.get(`https://api.github.com/users/${username}/repos`);
 const getgetStarCount = repos => {
-  return repos.data.reduce((count, repo) => {
-    return count + repo.stargazers_count;
-  }, 0);
+  return repos.data.reduce(
+    (count, { stargazers_count }) => count + stargazers_count,
+    0
+  );
 };
 
-const calculateScore = (profile, repos) => {
-  let followers = profile.followers;
-  let totalStars = getgetStarCount(repos);
-
-  return followers * 3 + totalStars;
+const calculateScore = ({ followers }, repos) => {
+  return followers * 3 + getgetStarCount(repos);
 };
 
 const handleError = error => {
@@ -29,27 +25,21 @@ const handleError = error => {
 };
 
 const getUserData = player => {
-  return axios.all([getProfile(player), getRepos(player)]).then(data => {
-    let profile = data[0];
-    let repos = data[1];
-
-    return {
+  return Promise.all([getProfile(player), getRepos(player)]).then(
+    ([profile, repos]) => ({
       profile,
       score: calculateScore(profile, repos)
-    };
-  });
+    })
+  );
 };
 
 const sortPlayers = players => {
-  return players.sort((a, b) => {
-    return b.score - a.score;
-  });
+  return players.sort((a, b) => b.score - a.score);
 };
 
 const Api = {
   battle: players => {
-    return axios
-      .all(players.map(getUserData))
+    return Promise.all(players.map(getUserData))
       .then(sortPlayers)
       .catch(handleError);
   },
@@ -59,9 +49,7 @@ const Api = {
       `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
     );
 
-    return axios.get(encodeURI).then(response => {
-      return response.data.items;
-    });
+    return axios.get(encodeURI).then(({ data }) => data.items);
   }
 };
 
